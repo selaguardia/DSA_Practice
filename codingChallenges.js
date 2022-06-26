3309cbec81d1c485ebf3ad35ded48ac778a22d59
selaguardia <selaguardia91@gmail.com>
2022-06-26 13:23:13 -0700

    reverse a string

---
+++ codingChallenges.js
@@ -0,0 +1,13 @@
+// reverse a string
+const reverseString = (string) => string.split("").reverse().join("");
+console.log(reverseString("Hello World"));
+
+// without built-in method
+const reverseString2 = (str) => {
+  let final = "";
+  for (let i = str.length -1; i >= 0; i--) {
+    final += str[i];
+  }
+  return final;
+};
+console.log(reverseString2("Hello World"));
