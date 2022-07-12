28099c1b6830e54a4ab960bc242f6a34868624b7
selaguardia <selaguardia91@gmail.com>
2022-07-12 10:33:06 -0700

    insert function into linkedList

--- Udemy/LinkedLists/linkedList.js
+++ Udemy/LinkedLists/linkedList.js
@@ -21,6 +21,13 @@ delete (O)n
 //     }
 //   }
 // }
+// Use a Node class, or just create inside the method like in lines 49-51
+class Node {
+  constructor(value) {
+    this.value = value,
+    this.next = null
+  }
+}
 
 class LinkedList {
   constructor(value) {
@@ -32,15 +39,12 @@ class LinkedList {
     this.length = 1;
   }
   append(value) {
-    const newNode = {
-      value: value,
-      next: null
-    };
+    const newNode = new Node(value);
     this.tail.next = newNode;
     this.tail = newNode;
     this.length++;
     return this;
-  }
+  } // O(1)
   prepend(value) {
     const newNode = {
       value: value,
@@ -50,6 +54,41 @@ class LinkedList {
     this.head = newNode;
     this.length++;
     return this;
+  } // O(1)
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+  insert(index, value) {
+    // check params
+    if (index >= this.length) {
+      return this.append(value);
+    }
+    const newNode = {
+      value: value,
+      next: null,
+    };
+    const leader = this.traverseToIndex(index - 1);
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this.printList();
+  }
+  traverseToIndex(index) {
+    // check params
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
   }
 }
 
@@ -57,4 +96,9 @@ const myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
 myLinkedList.prepend(1);
+console.log(myLinkedList.printList());
+myLinkedList.insert(2, 99);
+myLinkedList.insert(5, 49);
+myLinkedList.insert(72, 99);
+console.log(myLinkedList.printList());
 console.log(myLinkedList)
